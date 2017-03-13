940a49a0269017647a357f87e9fa126d45cdd33f
czb <czbnudt@gmail.com>
2017-03-13 15:50:36 +0800

    change to second suite

--- grails-app/controllers/simulatorcontest/RunnerController.groovy
+++ grails-app/controllers/simulatorcontest/RunnerController.groovy
@@ -9,7 +9,7 @@ class RunnerController {
 
     def testFile(UploadFile f) {
         def tester = new TestRunner(tarFileName:f.path, result:"")
-        def testSuite = new ParserTestSuite().getSuite("1") // get the first suite
+        def testSuite = new ParserTestSuite().getSuite("2") // get the second suite
         tester.test(testSuite, f.group)
         f.result = tester.result
         ///println tester.result
