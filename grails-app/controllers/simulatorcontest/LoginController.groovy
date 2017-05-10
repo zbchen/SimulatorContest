e61bbb41fedcb3618f150270f6ded6573631ec5a
czb <czbnudt@gmail.com>
2017-05-10 16:57:17 +0800

    enable login

--- grails-app/controllers/simulatorcontest/LoginController.groovy
+++ grails-app/controllers/simulatorcontest/LoginController.groovy
@@ -7,13 +7,13 @@ class LoginController {
         if (student) {
             if (student.group.password == params.get("password")) {
                 //login success, record in session
-                if (student.group.identity == 75) {
+                //if (student.group.identity == 75) {
                     session["group"] = student.group
                     session["student"] = student
                     redirect(uri: "/user")
-                } else {
-                    render "Login is not enabled now"
-                }
+//                } else {
+//                    render "Login is not enabled now"
+//                }
             } else {
                 render "Login Fails: password is incorrect"
             }
